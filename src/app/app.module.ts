66738d4236c3c7fef7e1f800e749b277b5610d79
homo-digitalis <secretary@homo-digitalis.org>
2019-06-17 11:30:54 +0200

    each day :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MondayComponent } from './monday/monday.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { TuesdayComponent } from './tuesday/tuesday.component';
+import { WednesdayComponent } from './wednesday/wednesday.component';
+import { ThursdayComponent } from './thursday/thursday.component';
+import { FridayComponent } from './friday/friday.component';
+import { SaturdayComponent } from './saturday/saturday.component';
+import { SundayComponent } from './sunday/sunday.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MondayComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    TuesdayComponent,
+    WednesdayComponent,
+    ThursdayComponent,
+    FridayComponent,
+    SaturdayComponent,
+    SundayComponent
   ],
   imports: [
     BrowserModule,
